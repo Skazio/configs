79ef16c8d7e7f27b033247b8a09032271a23e3dc
Skazio <unknown>
2022-03-13 13:07:33 +0100

    <feat>(peco): add peco ctrl+r shortcut

--- install-scripts/peco-install.sh
+++ install-scripts/peco-install.sh
@@ -6,3 +6,10 @@ sudo -u $SUDO_USER fish -c "omf install peco"
 # Install peco system-wide
 apt install -y peco
 
+# Add ctrl+r shortcut
+echo "
+function fish_user_key_bindings
+  bind \cr 'peco_select_history (commandline -b)'
+end
+" >> ~/.config/fish/config.fish
+
