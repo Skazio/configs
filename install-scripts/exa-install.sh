c235ddf435a51c7218268c390c59df41b7b85626
Skazio <unknown>
2022-03-13 11:56:30 +0100

    <fix>(exa): remove unwanted '/' that bugged the install

--- install-scripts/exa-install.sh
+++ install-scripts/exa-install.sh
@@ -1,9 +1,11 @@
 # Install exa
 curl -L https://github.com/ogham/exa/releases/download/v0.10.1/exa-linux-x86_64-v0.10.1.zip > exa-linux.zip
 unzip -d exa-linux/ exa-linux.zip
-mv /exa-linux/bin/* /usr/local/bin/
-mv /exa-linux/man/* /usr/share/man/man1/
-mv /exa-linux/completions/exa.fish /usr/share/fish/vendor_completions.d/
+mv exa-linux/bin/* /usr/local/bin/
+mv exa-linux/man/* /usr/share/man/man1/
+mv exa-linux/completions/exa.fish /usr/share/fish/vendor_completions.d/
+rm -r exa-linux/
+rm -r exa-linux.zip
 
 # Add alias
 echo "
