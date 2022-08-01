885abd60abef1329c2063f7e512fabd2d92cecb7
Skazio <47078492+Skazio@users.noreply.github.com>
2022-08-01 18:28:24 +0200

    fix(batcat): fix fish config file

--- install-scripts/bat-install.sh
+++ install-scripts/bat-install.sh
@@ -3,7 +3,12 @@
 # Install bat (may be aliased as batcat)
 apt install -y bat
 
-# depends of the system
-echo "alias cat='batcat'" >> $USER_HOME/.config/config.fish
-echo "alias cat='bat'" >> $USER_HOME/.config/config.fish
+# Depends of the system
+# On Ubuntu/Debian system
+echo "alias cat='batcat'" >> $USER_HOME/.config/fish/config.fish
+
+# On other system
+# echo "alias cat='bat'" >> $USER_HOME/.config/fish/config.fish
+
+>&2 echo -e "\033[33mIf this is not a Ubuntu/Debian distro, please change the alias in ~/.config/fish/config.fish from 'batcat' to simply 'bat'033[0m"
 
