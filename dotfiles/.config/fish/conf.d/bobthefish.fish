9c8963bff86ddfce7f6b7b16efe4712e88873807
Skazio <47078492+Skazio@users.noreply.github.com>
2024-09-12 21:32:55 +0200

    feat: improve icon prompt

--- dotfiles/.config/fish/conf.d/bobthefish.fish
+++ dotfiles/.config/fish/conf.d/bobthefish.fish
@@ -1,6 +1,6 @@
 set -g theme_color_scheme nord
 set -g theme_nerd_fonts yes
-set -g theme_newline_prompt "╰─> "
+set -g theme_newline_prompt "╰─󰒊 " # 󰁗 
 set -g theme_newline_cursor yes
 set -g fish_prompt_pwd_dir_length 0
 set -g theme_title_display_path no
--- dotfiles/.config/fish/functions/fish_mode_prompt.fish
+++ dotfiles/.config/fish/functions/fish_mode_prompt.fish
@@ -1,6 +1,6 @@
 function fish_mode_prompt
     set_color $fish_color_autosuggestion
-    echo "╭──  "
+    echo "╭─󰣠 "
     set_color normal
 end
 
