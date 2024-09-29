07d422d9587c471c47d013bec7e3497227adefaf
Skazio <47078492+Skazio@users.noreply.github.com>
2024-09-29 10:47:32 +0200

    feat: add nvim git keybindings

--- dotfiles/.config/nvim/lua/plugins/fugitive.lua
+++ dotfiles/.config/nvim/lua/plugins/fugitive.lua
@@ -1,7 +1,8 @@
 -- fugitive is a git wrapper
 return {
-	"tpope/vim-fugitive",
-	config = function()
-		vim.keymap.set("n", "<leader>gs", vim.cmd.Git)
-	end,
+    "tpope/vim-fugitive",
+    config = function()
+        vim.keymap.set("n", "<leader>gs", vim.cmd.Git)
+        vim.keymap.set("n", "<leader>gd", "<Esc>:Gvdiffsplit<CR>")
+    end,
 }
--- dotfiles/.config/nvim/lua/plugins/gitsigns.lua
+++ dotfiles/.config/nvim/lua/plugins/gitsigns.lua
@@ -1,9 +1,15 @@
 -- gitsigns display hint in sign column (add, modif, delete)
 -- it  also add hunks manipulation
 return {
-	"lewis6991/gitsigns.nvim",
-	event = { "BufReadPre", "BufNewFile" },
-	config = function()
-		require("gitsigns").setup()
-	end,
+    "lewis6991/gitsigns.nvim",
+    event = { "BufReadPre", "BufNewFile" },
+    config = function()
+        local gitsigns = require("gitsigns")
+
+        gitsigns.setup()
+
+        vim.keymap.set("v", "<leader>gs", function()
+            gitsigns.stage_hunk({ vim.fn.line("."), vim.fn.line("v") })
+        end)
+    end,
 }
