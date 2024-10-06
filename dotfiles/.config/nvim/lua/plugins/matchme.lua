15ca37918c6a09f4fac30b8e5ff47d624684b222
Skazio <47078492+Skazio@users.noreply.github.com>
2024-10-06 19:15:30 +0200

    nvim: add matchme.nvim plugin

--- dotfiles/.config/nvim/lua/plugins/matchme.lua
+++ dotfiles/.config/nvim/lua/plugins/matchme.lua
@@ -1,8 +1,8 @@
 -- matchme hightlight all the matching substrings that is selected in visual
 -- mode
 return {
-	dir = "/home/user/matchme.nvim",
-	config = function()
-		require("matchme")
-	end,
+    "skazio/matchme.nvim",
+    config = function()
+        require("matchme")
+    end,
 }
