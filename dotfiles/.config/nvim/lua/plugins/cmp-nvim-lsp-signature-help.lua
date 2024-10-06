531bcfbc379af7d533496f2e0ce07863f15d501f
Skazio <47078492+Skazio@users.noreply.github.com>
2024-10-06 19:17:16 +0200

    nvim: typo in doc

--- dotfiles/.config/nvim/lua/plugins/cmp-nvim-lsp-signature-help.lua
+++ dotfiles/.config/nvim/lua/plugins/cmp-nvim-lsp-signature-help.lua
@@ -1,5 +1,5 @@
 -- cmp-nvim-lsp-signature-help displays LSP signature while in insert mode
--- ie. show the paramter of a function with the current one being highlighted
+-- ie. show the parameter of a function with the current one being highlighted
 return {
     "hrsh7th/cmp-nvim-lsp-signature-help",
 }
