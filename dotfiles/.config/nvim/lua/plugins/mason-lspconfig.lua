33aee19725be14153a8b29ea357baadbf9e98dcb
Skazio <47078492+Skazio@users.noreply.github.com>
2025-01-04 12:42:43 +0100

    nvim: update mason config

--- dotfiles/.config/nvim/lua/plugins/mason-lspconfig.lua
+++ dotfiles/.config/nvim/lua/plugins/mason-lspconfig.lua
@@ -4,59 +4,72 @@
 -- different).
 -- It also provides auto-setup of those LSP servers
 return {
-	"williamboman/mason-lspconfig.nvim",
-	dependencies = { "williamboman/mason.nvim" },
+    "williamboman/mason-lspconfig.nvim",
+    dependencies = { "williamboman/mason.nvim" },
 
-	config = function()
-		local mason_lspconfig = require("mason-lspconfig")
+    config = function()
+        local mason_lspconfig = require("mason-lspconfig")
 
-		-- We need to call this here, to make sure lsp-zero execute only after
-		-- all LSP server are loaded and running
-		require("lsp-zero").extend_lspconfig()
+        -- We need to call this here, to make sure lsp-zero execute only after
+        -- all LSP server are loaded and running
+        require("lsp-zero").extend_lspconfig()
 
-		mason_lspconfig.setup()
+        mason_lspconfig.setup()
 
-		mason_lspconfig.setup_handlers({
-			-- default handler
-			function(server_name)
-				require("lspconfig")[server_name].setup({})
-			end,
+        mason_lspconfig.setup_handlers({
+            -- default handler
+            function(server_name)
+                require("lspconfig")[server_name].setup({})
+            end,
 
-			-- special handlers (override the default one for case-by-case)
-			["volar"] = function()
-				local vue_language_server_path = require("mason-registry")
-					.get_package("vue-language-server")
-					:get_install_path() .. "/node_modules/@vue/language-server"
+            -- special handlers (override the default one for case-by-case)
+            -- ["volar"] = function()
+            --   local vue_language_server_path = require("mason-registry")
+            --     .get_package("vue-language-server")
+            --     :get_install_path() .. "/node_modules/@vue/language-server"
+            --
+            --   local lspconfig = require("lspconfig")
+            --
+            --   lspconfig.ts_ls.setup({
+            --     init_options = {
+            --       plugins = {
+            --         {
+            --           name = "@vue/typescript-plugin",
+            --           location = vue_language_server_path,
+            --           languages = { "vue" },
+            --         },
+            --       },
+            --     },
+            --     filetypes = { "typescript", "javascript", "javascriptreact", "typescriptreact", "vue" },
+            --   })
+            --
+            --   lspconfig.volar.setup({})
+            -- end,
 
-				local lspconfig = require("lspconfig")
+            ["volar"] = function()
+                local lspconfig = require("lspconfig")
 
-				lspconfig.ts_ls.setup({
-					init_options = {
-						plugins = {
-							{
-								name = "@vue/typescript-plugin",
-								location = vue_language_server_path,
-								languages = { "vue" },
-							},
-						},
-					},
-					filetypes = { "typescript", "javascript", "javascriptreact", "typescriptreact", "vue" },
-				})
+                lspconfig.volar.setup({
+                    filetypes = { "typescript", "javascript", "javascriptreact", "typescriptreact", "vue" },
+                    init_options = {
+                        vue = {
+                            hybridMode = false,
+                        },
+                    },
+                })
+            end,
 
-				lspconfig.volar.setup({})
-			end,
-
-			["lua_ls"] = function()
-				require("lspconfig").lua_ls.setup({
-					settings = {
-						Lua = {
-							diagnostics = {
-								globals = { "vim" },
-							},
-						},
-					},
-				})
-			end,
-		})
-	end,
+            ["lua_ls"] = function()
+                require("lspconfig").lua_ls.setup({
+                    settings = {
+                        Lua = {
+                            diagnostics = {
+                                globals = { "vim" },
+                            },
+                        },
+                    },
+                })
+            end,
+        })
+    end,
 }
